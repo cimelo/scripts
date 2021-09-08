deaff6e7d097b9f9ac6141b132c75d5148d65b10
ianm <caio.ian@ufpe.br>
2021-09-08 20:27:33 -0300

    ffmpeg encoder

--- ffencode.sh
+++ ffencode.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/sh
 
-echo "Input"
-read I
-echo "Bitrate"
-read B
-echo "Output"
-read O
+echo "Input order: input file; video bitrate; output file"
 
 ffmpeg -i $1 -c:v libvpx -c:a libvorbis -b:v $2 "$3"

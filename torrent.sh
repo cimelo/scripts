833cabbec730f22124934a509d38632bf47441d3
ianm <caio.ian@ufpe.br>
2021-10-01 16:04:00 -0300

    Removing files

--- torrent.sh
+++
@@ -1,6 +0,0 @@
-R=$(printf "%s\n%s" "Remote" "Local" | dmenu -p "Download Type > " -i)
-
-case $R in
-	Remote)torrent_handler.sh "192.168.0.175:9091" "ianm:DyFUj6SwrQiDI8G" "$1" ;;
-	Local)torrent_handler.sh "" "" "$1" ;;
-esac
